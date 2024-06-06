1df2f536cc58b2fb459c8fb179ddbc997933c1a2
Emanuele Raimondo <140118231+LeleFox@users.noreply.github.com>
2024-06-06 13:32:39 +0200

    vision transformers

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 from models.RGB_CNN import RGB_ResNet18, RGB_ResNet50
 from models.DEPTH_CNN import DEPTH_ResNet18, DEPTH_ResNet50
-from models.MESH_CNN import MESH_CNN
-from models.VOXEL_CNN import VOXEL_CNN
+from models.RGB_ViT import RGB_ViT
+from models.DEPTH_ViT import DEPTH_ViT
 from models.FUSION_net import feature_FUSION_net, Attention_Fusion_CNN
