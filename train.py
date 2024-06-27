c27ff3fdccd045ba591646e18e38f19c9df5f8a7
Emanuele Raimondo <140118231+LeleFox@users.noreply.github.com>
2024-06-27 10:55:36 +0200

    local changes

--- train.py
+++ train.py
@@ -48,8 +48,8 @@ def main():
     
     #!check gpu
     #check0 = os.environ['CUDA_VISIBLE_DEVICES'] 
-    check = torch.cuda.is_available()
-    torch.zeros(1).cuda()
+    #check = torch.cuda.is_available()
+    #torch.zeros(1).cuda()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     #!TRANSFORMATIONS and AUGMENTATION for TRAINING samples, 
