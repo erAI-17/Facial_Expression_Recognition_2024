small fixes, lighter computation

models/AttentionFusion.py
@@ -12,13 +12,15 @@ class AttentionFusion1D_Module(nn.Module):
    def __init__(self, rgb_dim, depth_dim, d_model, nhead, d_ff):
       super(AttentionFusion1D_Module, self).__init__()
       self.proj_rgb = nn.Linear(rgb_dim, d_model)
-      self.proj_depth = nn.BatchNorm1d(depth_dim, d_model)
+      self.proj_depth = nn.Linear(depth_dim, d_model)
       self.multihead_attn = nn.MultiheadAttention(embed_dim=d_model, num_heads=nhead)
       self.ffn = nn.Sequential(
          nn.Linear(d_model, d_ff),
          nn.ReLU(),
          nn.Linear(d_ff, d_model)
       )
+      #?LayerNorm normalizes across the features for each individual sample.
+      #?BatchNorm normalizes across the batch for each feature.
       self.layer_norm = nn.LayerNorm(d_model)
       self.d_ff = d_ff
 
@@ -50,7 +52,7 @@ class AttentionFusion1D(nn.Module):
       self.rgb_model = rgb_model
       self.depth_model = depth_model
       
-      self.attention = AttentionFusion1D_Module(768, 512, d_model=512, nhead=8, d_ff=1024)
+      self.attention = AttentionFusion1D_Module(768, 512, d_model=512, nhead=4, d_ff=1024)
       self.fc = nn.Linear(512, num_classes) 
 
    def forward(self, x):
@@ -60,7 +62,6 @@ class AttentionFusion1D(nn.Module):
       # Apply attention
       att_fused_feat = self.attention(rgb_feat['late_feat'], depth_feat['late_feat'])  
 
-      #!no batch norms
       x = self.fc(att_fused_feat)
 
       return x, {}