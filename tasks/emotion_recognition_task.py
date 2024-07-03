66465f55814ada2dd95e40d2b63e727836457310
Emanuele Raimondo <140118231+LeleFox@users.noreply.github.com>
2024-07-03 22:24:52 +0200

    trial

--- tasks/emotion_recognition_task.py
+++ tasks/emotion_recognition_task.py
@@ -77,15 +77,13 @@ class EmotionRecognition(tasks.Task, ABC):
             #? model_args[m].lr : Initial learning rate for the optimizer
             #? weight_decay : The weight decay (L2 penalty) for the optimizer. 
             #!ADAM
-            # self.optimizer[m] = torch.optim.Adam(optim_params[m], model_args[m].lr,
-            #                                     weight_decay=model_args[m].weight_decay)
-            # # Use a learning rate scheduler to decrease the learning rate over time
+            self.optimizer[m] = torch.optim.Adam(optim_params[m], model_args[m].lr,
+                                                weight_decay=model_args[m].weight_decay)
             
             #!SGD with momentum
-            self.optimizer[m] = torch.optim.SGD(optim_params[m], model_args[m].lr,
-                                                weight_decay=model_args[m].weight_decay,
-                                                momentum=model_args[m].sgd_momentum)
-            #Use a learning rate scheduler to decrease the learning rate over time
+            # self.optimizer[m] = torch.optim.SGD(optim_params[m], model_args[m].lr,
+            #                                     weight_decay=model_args[m].weight_decay,
+            #                                     momentum=model_args[m].sgd_momentum)
             
             #!LR schedulers
             #debugging schedule
