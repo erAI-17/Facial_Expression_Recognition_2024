fixed Center updates,changed normalization values

Dataset_Initializer.py
@@ -48,7 +48,7 @@ def train_test_annotations(test_size):
                     
                     
                     new_entry = [dataset, subj_id, code, description_label, emotions[description_label], add]
-                    if new_entry not in data:
+                    if new_entry not in data: #avoid duplicates (same sample with different modalities)
                         data.append([dataset, subj_id, code, description_label, emotions[description_label], add])
 
                     #!update class distribution
@@ -64,14 +64,14 @@ def train_test_annotations(test_size):
                         img = img / 255.0  # Normalize to [0, 1]
                         
                         #!update mean and std
-                        sum_pix[m] += np.sum(img, axis=(0, 1))
+                        sum_pix[m] += np.sum(img, axis=(0, 1)) #sum all pixels in the image, separately for each channel (black pixels are 0)
                         sum_sq_pix[m] += np.sum(img ** 2, axis=(0, 1))
                        
-                        # Create a mask where all three channels are below the threshold
-                        mask = (img[:, :, 0] > 0) & (img[:, :, 1] >  0) & (img[:, :, 2] > 0)
+                        # Create a mask to maintain only pixels where all three channels are below the threshold
+                        mask = (img[:, :, 0] > 0) | (img[:, :, 1] >  0) | (img[:, :, 2] > 0)
                         #mask off 0 values (black pixels) in the frame, from each channel
                         img = img[mask]
-                        n_pix[m] += img.shape[0] #mask converts into a 1D array
+                        n_pix[m] += img.shape[0] #mask converts into a 2D array
                         
                     elif m == 'Depth':
                         img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
@@ -80,17 +80,17 @@ def train_test_annotations(test_size):
                         
                         max_depth = max(max_depth, img.max()) #!get max depth before normalization
                         
-                        img = img / 9785.0  # Normalize to [0, 1]
+                        img = img / 9785.0  # Normalize to [0, 1] using max_depth=9785
                         
                         #!update mean and std
                         sum_pix[m] += np.sum(img, axis=(0, 1))
                         sum_sq_pix[m] += np.sum(img ** 2, axis=(0, 1))
                         
                         # Create a mask for each channel where pixel values are greater than the threshold=0 (to avoid balck frame pixels)
-                        mask = (img[:, :, 0] > 0) & (img[:, :, 1] >  0) & (img[:, :, 2] > 0)
+                        mask = (img[:, :, 0] > 0) | (img[:, :, 1] >  0) | (img[:, :, 2] > 0)
                         #mask off 0 values (black pixels) in the frame, from each channel
                         img = img[mask]
-                        n_pix[m] += img.shape[0] #mask converts into a 1D array
+                        n_pix[m] += img.shape[0] #mask converts into a 2D array
                                         
     # Average mean and std over the number of samples
     for m in ['Color', 'Depth']:

models/LocalGlobalFusionNet.py
@@ -73,8 +73,8 @@ class LocalGlobalFusionNet(nn.Module):
       self.proj_layers = nn.ModuleDict()
       
       for stage in self.stages.keys():
-            self.Att_map_RGB[stage] = nn.Conv2d(self.stages[stage][0], 1, kernel_size=1, bias=False) #1 #self.stages[stage][0]
-            self.Att_map_D[stage] = nn.Conv2d(self.stages[stage][0], 1, kernel_size=1, bias=False)
+            self.Att_map_RGB[stage] = nn.Conv2d(self.stages[stage][0], self.stages[stage][0], kernel_size=1, bias=False) #1 #self.stages[stage][0]
+            self.Att_map_D[stage] = nn.Conv2d(self.stages[stage][0], self.stages[stage][0], kernel_size=1, bias=False)
             #self.LocalAttention_Module[stage] = LocalAttention_Module(self.stages[stage][0])
             #self.GlobalAttention_Module[stage] = GlobalAttention_Module(self.stages[stage][0])
             

tasks/emotion_recognition_task.py
@@ -6,7 +6,7 @@ import tasks
 import numpy as np
 from typing import Dict, Tuple
 from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts, OneCycleLR
-from utils.losses import FocalLoss, CenterLoss, CE_Center_Criterion
+from utils.losses import FocalLoss, CenterLoss, IslandLoss, CE_Center_Criterion, CE_Center_Island_Criterion
 from utils.args import args
 
 
@@ -62,11 +62,20 @@ class EmotionRecognition(tasks.Task, ABC):
             self.criterion = FocalLoss(alpha=1, gamma=2, reduction='mean')
         elif args.train.loss_fn == 'CE_Center':    
             #!CEL+Center Loss 
-            CE_loss = torch.nn.CrossEntropyLoss(weight=self.class_weights, reduction='mean')
-            Center_loss = CenterLoss(feat_dim=1408) #1408 #2816 
-            lambda_center = 3e-3 #5e-4 #3e-3
-            self.optimizer_centers = torch.optim.SGD(Center_loss.parameters(), lr=0.5)  #alpha (lr) for class centers
-            self.criterion = CE_Center_Criterion(CE_loss, Center_loss, lambda_center)
+            self.CE_loss = torch.nn.CrossEntropyLoss(weight=self.class_weights, reduction='mean')
+            self.Center_loss = CenterLoss(feat_dim=1408) #1408 #2816 
+            self.lambda_center = 3e-3 #5e-4/2 #3e-3/2
+            self.optimizer_centers = torch.optim.SGD(self.Center_loss.parameters(), lr=0.5)  #alpha (lr) for class centers
+            self.criterion = CE_Center_Criterion(self.CE_loss, self.Center_loss, self.lambda_center)
+        elif args.train.loss_fn == 'CE_Center_Island':
+            #!CEL+Center Loss + Island Loss
+            self.CE_loss = torch.nn.CrossEntropyLoss(weight=self.class_weights, reduction='mean')
+            self.Island_loss = IslandLoss(feat_dim=1408)
+            self.lambda_global = 3e-3 #5e-4/2 #3e-3/2
+            self.lambda_island = 0.1
+            self.optimizer_centers = torch.optim.SGD(self.Island_loss.parameters(), lr=0.5)  #alpha (lr) for class centers
+            self.criterion = CE_Center_Island_Criterion(self.CE_loss, self.Island_loss, self.lambda_global, self.lambda_island)
+            
             
         # Initialize the model parameters and the optimizer
         optim_params = {}
@@ -194,6 +203,9 @@ class EmotionRecognition(tasks.Task, ABC):
                 
         #! If using center loss, perform the step with the center optimizer
         if args.train.loss_fn == 'CE_Center':
+            #don't want the lambda_center to affect the gradients of the loss with respect to the centers, so rescale back the gradients
+            for param in self.Center_loss.parameters():
+                param.grad.data *= (1. / self.lambda_center) 
             self.optimizer_centers.step()
             
         if args.amp: #! Update the scaler for the next iteration        
@@ -222,6 +234,15 @@ class EmotionRecognition(tasks.Task, ABC):
         """Reset the gradient when gradient accumulation is finished."""
         for m in self.modalities:
             self.optimizer[m].zero_grad(set_to_none=True)
+        
+        if args.train.loss_fn == 'CE_Center':
+            self.optimizer_centers.zero_grad(set_to_none=True)
+            
+        if args.train.loss_fn == 'CE_Center_Island':
+            self.optimizer_centers.zero_grad(set_to_none=True)
+            self.optimizer_island.zero_grad(set_to_none=True)
+        
+        
             
             
     def grad_clip(self):

train.py
@@ -34,7 +34,8 @@ torch.manual_seed(13696641)
 
 #!tensorboard visualization command
 #   tensorboard --logdir=logs
-    
+
+
 def init_operations():
     logger.info("Running with parameters: " + pformat_dict(args, indent=1))
 
@@ -271,7 +272,7 @@ def train(emotion_classifier, train_loader, val_loader, device):
             #! every  N_val_visualize  validations, also visualize features and GRADCAM
             if real_iter % (args.train.eval_freq*args.N_val_visualize)==0:
                 visualize_features(emotion_classifier, val_loader, device, int(real_iter))
-                #compute_gradcam(emotion_classifier, val_loader, device, int(real_iter))
+                compute_gradcam(emotion_classifier, val_loader, device, int(real_iter))
 
             emotion_classifier.save_model(real_iter, val_metrics['top1'], prefix=None)
             emotion_classifier.train(True) 

utils/losses.py
@@ -16,7 +16,20 @@ class CE_Center_Criterion(nn.Module):
     def forward(self, logits, labels, features):
         ce_loss_value = self.ce_loss(logits, labels)
         center_loss_value = self.center_loss(features, labels)
-        total_loss = ce_loss_value + self.lambda_center/2 * center_loss_value
+        total_loss = ce_loss_value + self.lambda_center * center_loss_value
+        return total_loss
+
+class CE_Center_Island_Criterion(nn.Module):
+    def __init__(self, ce_loss=None, center_loss=None, island_loss=None, lambda_center=5e-4, lambda_island=0.1):
+        super(CE_Center_Island_Criterion, self).__init__()
+        self.ce_loss = ce_loss
+        self.island_loss = island_loss
+        self.lambda_global = lambda_center
+
+    def forward(self, logits, labels, features):
+        ce_loss_value = self.ce_loss(logits, labels)
+        island_loss_value = self.island_loss(features, labels)
+        total_loss = ce_loss_value + self.lambda_global * island_loss_value
         return total_loss
 
 #!CENTER LOSS
@@ -55,7 +68,56 @@ class CenterLoss(nn.Module):
         loss = dist.clamp(min=1e-12, max=1e+12).sum() / batch_size #? sum over batch_size and compute mean
         return loss
     
+#!!!ISLAND LOSS
+class IslandLoss(nn.Module):
+    def __init__(self, feat_dim):
+        super(IslandLoss, self).__init__()
+        self.num_classes, _ = utils.utils.get_domains_and_labels(args)
+        self.feat_dim = feat_dim
+        if torch.cuda.is_available():
+            self.centers = nn.Parameter(torch.randn(self.num_classes, self.feat_dim).cuda())
+        else:
+            self.centers = nn.Parameter(torch.randn(self.num_classes, self.feat_dim))
+    
+    def forward(self, x, labels):
+        """
+        Args:
+            x: feature matrix (batch_size, feat_dim).
+            labels: ground truth labels with shape (batch_size).
+        """
+        batch_size = x.size(0)
+        
+        #!Center loss (INTRA-CLASS compactness)
+        #?||x - c||^2 = ||x||^2 + ||c||^2 - (2 * x * c)
+        distmat = torch.pow(x, 2).sum(dim=1, keepdim=True).expand(batch_size, self.num_classes) + \
+                  torch.pow(self.centers, 2).sum(dim=1, keepdim=True).expand(self.num_classes, batch_size).t()
+        distmat.addmm_(mat1=x, mat2=self.centers.t(), beta=1, alpha=-2)
+        
+        classes = torch.arange(self.num_classes).long()
+        if torch.cuda.is_available(): 
+            classes = classes.cuda()
+            
+        labels = labels.unsqueeze(1).expand(batch_size, self.num_islands, self.num_classes)
+        mask = labels.eq(classes.expand(batch_size, self.num_islands, self.num_classes))
 
+        dist = distmat * mask.float()
+        center_loss = dist.clamp(min=1e-12, max=1e+12).sum() / batch_size #? sum over batch_size and compute mean
+        
+        #!Island loss (INTER-CLASS separation)
+        #?sum_{i=1}^{K} sum_{j=1, j!=i}^{K} [(c_i * c_j)/(||c_i|| * ||c_j||) +1]
+        norm_centers = torch.norm(self.centers, p=2, dim=1, keepdim=True)
+        cos_similarity_matrix = torch.mm(self.centers, self.centers.t()) / (torch.mm(norm_centers, norm_centers.t()) + 1e-12)
+        
+        # Set the diagonal to 0 to exclude self-similarity from the loss calculation
+        cos_similarity_matrix.fill_diagonal_(0)
+        
+        # Apply the island loss formula
+        island_loss = (cos_similarity_matrix + 1).sum()
+        
+        loss = center_loss + self.lambda_island * island_loss
+        
+        return loss
+        
 #!!!FOCAL LOSS
 class FocalLoss(nn.Module):
     def __init__(self,

utils/transforms.py
@@ -8,8 +8,8 @@ import cv2
 ImageNet_mean = [0.485, 0.456, 0.406] 
 ImageNet_std = [0.229, 0.224, 0.225]
 
-Calder_Mendes_mean_RGB = [0.4995032053305857, 0.3682551044795388, 0.32203149433857986]
-Calder_Mendes_std_RGB = [0.26163392534341473, 0.2138249487001331, 0.21422498153220476]
+Calder_Mendes_mean_RGB = [0.499013695404109, 0.3678942168471979, 0.3217159055466871]
+Calder_Mendes_std_RGB = [0.26197232721001346, 0.21403053699700778, 0.2143569416789142]
 Calder_Mendes_mean_DEPTH = [0.36479503953065234, 0.36479503953065234, 0.36479503953065234]
 Calder_Mendes_std_DEPTH = [0.06903268, 0.06903268, 0.06903268]
    
@@ -30,10 +30,12 @@ class ToTensorUint16:
         img_tensor = torch.from_numpy(img_np).permute(2, 0, 1)
         return img_tensor
         
+        
 class RGBTransform:
     def __init__(self, augment=False):
         to_tensor = [
-            transforms.ToTensor(), #normalize to [0,1]
+                transforms.ToImage(), #transform to PIL image
+                transforms.ToDtype(torch.float32, scale=True) #convert to float32 and scale to [0,1] (deviding by 255)
         ]
         normalization = [
             transforms.Normalize(mean=Calder_Mendes_mean_RGB, std=Calder_Mendes_std_RGB)  # Normalize the tensor to [-1,1]
@@ -56,10 +58,11 @@ class RGBTransform:
         
         transformations = resizing + augmentations + to_tensor + normalization       
         self.transform = transforms.Compose(transformations)
-    
+            
     def __call__(self, img):
         # Convert the image from BGR to RGB
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                
         # Convert NumPy array to PIL Image
         if isinstance(img, np.ndarray):
             img = Image.fromarray(img)