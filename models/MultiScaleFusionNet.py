254493ebc98fe70bdc0de481987fe9063acf404e
Emanuele Raimondo <140118231+LeleFox@users.noreply.github.com>
2024-08-20 13:11:37 +0200

    patches generation update

--- models/MultiScaleFusionNet.py
+++ models/MultiScaleFusionNet.py
@@ -26,7 +26,24 @@ class SpatialAttentionModule(nn.Module):
       L = self.sigmoid(self.conv2(self.relu(self.conv1(X)))) #?[batch_size, 1, H, W]
   
       return L
+
+
+class PatchEmbedding(nn.Module):
+   def __init__(self, H, patch_size, in_channels, embed_dim=768):
+      super(PatchEmbedding, self).__init__()
+      self.H = H
+      self.patch_size = patch_size
+      self.num_patches = (H // patch_size) ** 2
+      self.embed_dim = embed_dim
+      
+      # Flatten and linear projection for each patch
+      self.proj = nn.Conv2d(in_channels, embed_dim, kernel_size=patch_size, stride=patch_size)
    
+   def forward(self, x):
+      x = self.proj(x)  # (B, embed_dim, H/P, W/P)
+      x = x.flatten(2)  # (B, embed_dim, num_patches)
+      x = x.transpose(1, 2)  # (B, num_patches, embed_dim)
+      return x
 
 class MultiScaleFusionNet(nn.Module):
    """Visual Transformer Feature Fusion with Attention Selective fusion.
@@ -41,8 +58,12 @@ class MultiScaleFusionNet(nn.Module):
       
       #? Heights and Widths of the feature maps at different stages 
       self.stages = {'late': [352, 9]} # only late features
-      #self.stages = {'early': [32, 130], 'mid': [88, 17], 'late': [352, 9]} 
+      #self.stages = {'early': [32, 130], 'mid': [88, 17], 'late': [352, 9]}
       
+      #? Weights for RGB and Depth modalities
+      self.weight_rgb = nn.ParameterDict({stage: nn.Parameter(torch.randn(1)) for stage in self.stages})
+      self.weight_depth = nn.ParameterDict({stage: nn.Parameter(torch.randn(1)) for stage in self.stages}) 
+            
       self.patch_sizes = { 'early': 10 , 'mid': 8, 'late': 1}
       self.n_spatial_attentions = 3
       self.patch_size = {}
@@ -54,16 +75,20 @@ class MultiScaleFusionNet(nn.Module):
          'depth': nn.ModuleDict({stage: nn.ModuleList([SpatialAttentionModule(self.stages[stage][0]) for _ in range(self.n_spatial_attentions)]) for stage in self.stages})
       })
       
-      self.proj_layers = nn.ModuleDict({stage: nn.Linear((self.patch_sizes[stage])**2 * self.stages[stage][0], 768) for stage in self.stages})
+      #patches embeddings
+      self.patch_embed = nn.ModuleDict({
+         'rgb': nn.ModuleDict({stage: PatchEmbedding(self.stages[stage][1], self.patch_sizes[stage], self.stages[stage][0]) for stage in self.stages}),
+         'depth': nn.ModuleDict({stage: PatchEmbedding(self.stages[stage][1], self.patch_sizes[stage], self.stages[stage][0]) for stage in self.stages})
+      })
 
       #? Transformer encoder   
       self.Ct = 768
       self.trans_encoder_layer = nn.TransformerEncoderLayer(self.Ct, nhead=8, dim_feedforward=3072, activation='gelu', batch_first=True)
       self.transformer_encoder = nn.TransformerEncoder(self.trans_encoder_layer, num_layers=4)
       self.cls_token = nn.Parameter(torch.zeros(1, 1,  self.Ct))
-      nn.init.normal_(self.cls_token, std=0.02)  # Initialize with small random values to break symmetry
+      #nn.init.normal_(self.cls_token, std=0.02)  # Initialize with small random values to break symmetry
       self.pos_embed = nn.Parameter(torch.zeros(1,  sum([(self.stages[stage][1] // self.patch_sizes[stage])**2 for stage in self.stages]) + 1,  self.Ct))
-      nn.init.normal_(self.pos_embed, std=0.02)  # Initialize with small random values to break symmetry
+      #nn.init.normal_(self.pos_embed, std=0.02)  # Initialize with small random values to break symmetry
       
       #? final classification
       self.fc = nn.Linear(self.Ct, num_classes)
@@ -76,6 +101,7 @@ class MultiScaleFusionNet(nn.Module):
 
       for m in ['rgb', 'depth']:          
          for stage in self.stages:
+            
             #? apply multiple spatial attention modules and perform maxpooling
             X_att = torch.zeros(X[m][stage].size(0), 0, X[m][stage].size(2), X[m][stage].size(3)).to(X[m][stage].device)
             for spatial_attetion_module in self.SpatialAttentionModules[m][stage]:
@@ -89,16 +115,11 @@ class MultiScaleFusionNet(nn.Module):
             
             X[m][stage] = X_att * X[m][stage] #?apply attention to features
          
-            #? Extract patches and flatten into sequence like input for transformer
-            patches = X[m][stage].unfold(2, self.patch_sizes[stage], self.patch_sizes[stage]).unfold(3, self.patch_sizes[stage], self.patch_sizes[stage]) #? [batch_size, C, H, W] -> [batch_size, C, H//patch_size, W//patch_size, patch_size, patch_size]
-            patches = patches.contiguous().view(patches.size(0), patches.size(1), -1, self.patch_sizes[stage], self.patch_sizes[stage])  #? [batch_size, C, H*W (num_patches), patch_size, patch_size]
-            X[m][stage] = patches.view(patches.size(0), -1, self.patch_sizes[stage] * self.patch_sizes[stage] * patches.size(1)) #? [batch_size, H*W, C*patch_size*patch_size]
-            
-            #? Project features to transformer dimension
-            X[m][stage] = self.proj_layers[stage](X[m][stage]) #? apply projection layer
+            #?patch embeddings
+            X[m][stage] = self.patch_embed[m][stage](X[m][stage])
          
       #?concatenate the modalitites   
-      X_fused_global = torch.cat([X['rgb'][stage] + X['depth'][stage] for stage in self.stages], dim=1)
+      X_fused_global = torch.cat([X['rgb'][stage]*self.weight_rgb[stage] + X['depth'][stage]*self.weight_depth[stage] for stage in self.stages], dim=1)
       
       #?prepend [cls] token as learnable parameter
       cls_tokens = self.cls_token.expand(X_fused_global.size(0), -1, -1) # (batch_size, 1, C)
